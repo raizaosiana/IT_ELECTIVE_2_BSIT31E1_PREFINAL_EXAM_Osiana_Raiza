13dfb058d56695116d4ec19d83ac7e2fdd3014f3
raizaosiana <raizaosiana28@gmail.com>
2026-09-12 10:00:11 +0800

    Adding ExamController, exam item and index

---
+++ IT_ELECTIVE_2_BSIT31E1_PREFINAL_EXAM_Osiana_Raiza/Models/ExamItem.cs
@@ -0,0 +1,10 @@
+﻿namespace IT_ELECTIVE_2_BSIT31E1_PREFINAL_EXAM_Osiana_Raiza.Models;
+
+public class ExamItem
+{
+    public int Id { get; set; }
+    public string Question { get; set; } = string.Empty;
+    public List<string> Options { get; set; } = [];
+    public string CorrectAnswer { get; set; } = string.Empty;
+    public string Explanation { get; set; } = string.Empty;
+}
